f36202d47c80438885805da247b5a4071c96d798
Alex Mayer <amayer5125@gmail.com>
2017-12-01 01:11:33 -0500

    2017 Day 01

---
+++ 2017/day-01/part2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var d string
+	var nums []int
+	var total int
+
+	// get the input
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		d = scanner.Text()
+	}
+
+	// put the input into an integer slice
+	for i := 0; i < len(d); i++ {
+		c, _ := strconv.Atoi(d[i : i+1])
+		nums = append(nums, c)
+	}
+
+	// loop through each integer
+	for k, v := range nums {
+		// get the next index
+		n := (k + (len(d) / 2)) % len(d)
+
+		// check if the value is equal to the next index
+		if v == nums[n] {
+			total += v
+		}
+	}
+
+	fmt.Println(total)
+}
