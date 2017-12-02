ae68deb901d66539f0aa5903c3f8ab8fa9877855
Alex Mayer <amayer5125@gmail.com>
2017-12-02 01:03:21 -0500

    2017 Day 01: Optimizations
    Condense to one internal loop
    Only convert string to integer if the strings match
    Part 2 only loop through half of the slice
        Since we already check values half way around only loop
            through half of them
        Multiply value by 2 instead of iterating over it again

--- 2017/day-01/part1.go
+++ 2017/day-01/part1.go
@@ -9,31 +9,23 @@ import (
 
 func main() {
 	var d string
-	var nums []int
-	var total int
+	var t int
 
 	// get the input
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		d = scanner.Text()
-	}
 
-	// put the input into an integer slice
-	for i := 0; i < len(d); i++ {
-		c, _ := strconv.Atoi(d[i : i+1])
-		nums = append(nums, c)
-	}
+		// loop through the input
+		for i := 0; i < len(d); i++ {
+			n := (i + 1) % len(d)
 
-	// loop through each integer
-	for k, v := range nums {
-		// get the next index
-		n := (k + 1) % len(d)
-
-		// check if the value is equal to the next index
-		if v == nums[n] {
-			total += v
+			if d[i:i+1] == d[n:n+1] {
+				c, _ := strconv.Atoi(d[i : i+1])
+				t += c
+			}
 		}
 	}
 
-	fmt.Println(total)
+	fmt.Println(t)
 }
--- 2017/day-01/part2.go
+++ 2017/day-01/part2.go
@@ -9,31 +9,27 @@ import (
 
 func main() {
 	var d string
-	var nums []int
-	var total int
+	var t int
 
 	// get the input
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		d = scanner.Text()
-	}
 
-	// put the input into an integer slice
-	for i := 0; i < len(d); i++ {
-		c, _ := strconv.Atoi(d[i : i+1])
-		nums = append(nums, c)
-	}
+		// get the half length
+		h := len(d) / 2
 
-	// loop through each integer
-	for k, v := range nums {
-		// get the next index
-		n := (k + (len(d) / 2)) % len(d)
+		// loop through half the string
+		// if they didn't match the first time they will not match the second
+		for i := 0; i < h; i++ {
+			n := (i + h) % len(d)
 
-		// check if the value is equal to the next index
-		if v == nums[n] {
-			total += v
+			if d[i:i+1] == d[n:n+1] {
+				c, _ := strconv.Atoi(d[i : i+1])
+				t += c * 2
+			}
 		}
 	}
 
-	fmt.Println(total)
+	fmt.Println(t)
 }
