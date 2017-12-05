1c87da8e47b3ea0af46bd7a13f4cacc12357b7f6
Alex Mayer <amayer5125@gmail.com>
2017-12-04 20:59:32 -0500

    Optimize 2017 Day 03
    Use math to calculate the numbers position instead of loops

--- 2017/day-03/part1.go
+++ 2017/day-03/part1.go
@@ -8,40 +8,17 @@ import (
 )
 
 func main() {
-	var d, rl, r, s int
-
 	// get the input
-	d, _ = strconv.Atoi(os.Args[1])
+	d, _ := strconv.ParseFloat(os.Args[1], 64)
 
-	// get the row length (1/4 of swirl square)
-	// since distance to center is a square all 4 sides have the same distance values
-	rl = int(math.Floor(math.Sqrt(float64(d))))
-	if rl < 2 {
-		rl = 2
-	}
+	// distance from the center to the swirl our number is in
+	radius := math.Floor(math.Ceil(math.Sqrt(d)) / 2)
 
-	// how many rows we are out from the center of the swirl
-	r = int(math.Ceil(float64(rl) / 2))
+	// first number in swirl row
+	row_start := math.Pow(2*radius-1, 2) + 1
 
-	// get the first number in the swirl row
-	// start at 2 because first row starts with 2
-	s = 2
-	for i := 1; i < r; i++ {
-		s += i * 8
-	}
+	// distance from a cardinal direction (n,s,e,w)
+	dist_from_cardinal := math.Abs(math.Mod(d-row_start, 2*radius) - (radius - 1))
 
-	// slice of values coresponding to the distance to the center of the square
-	dtc := []int{rl - 1}
-	// fill the values with the distance to the center
-	for j := 0; j < rl-1; j++ {
-		adder := -1
-		if j >= (rl-1)/2 {
-			adder = 1
-		}
-
-		dtc = append(dtc, dtc[len(dtc)-1]+adder)
-	}
-
-	// get output our starting numbers coresponding slot in the distance to center slice
-	fmt.Println(dtc[(d-s)%rl])
+	fmt.Println(radius + dist_from_cardinal)
 }
